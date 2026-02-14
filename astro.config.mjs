5354feece1c85eddfed510c0572afdc1d011af67
Christopher Tomasulo <docsulo@gmail.com>
2026-02-13 22:15:15 -0500

    Switch to static build, remove Cloudflare adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,8 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 
-import cloudflare from '@astrojs/cloudflare';
-
 // https://astro.build/config
 export default defineConfig({
   site: 'https://dhammarato.com',
   integrations: [mdx(), sitemap()],
-  adapter: cloudflare(),
 });
